ee1f4b0c418dc6dc4b8c2cad9d621e1d8fada09c
Isaac <carlin_isaac@hotmail.com>
2022-10-17 22:17:40 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mi-tienda-ect',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
